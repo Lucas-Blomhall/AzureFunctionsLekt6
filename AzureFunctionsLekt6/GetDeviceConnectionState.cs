c6a2ff0f4d3fd5402065fd3fc24bd19133b76fa3
Lucas <lucas.blomhall@gmail.com>
2022-09-09 14:15:55 +0200

    Add project files.

---
+++ AzureFunctionsLekt6/GetDeviceConnectionState.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFunctionsLekt6
+{
+    public static class GetDeviceConnectionState
+    {
+        private static readonly RegistryManager registryManager =
+            RegistryManager.CreateFromConnectionString(Environment.GetEnvironmentVariable("IotHub"));
+
+        [FunctionName("GetDeviceConnectionState")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "devices/connect")] HttpRequest req,
+            ILogger log)
+        {
+
+            string deviceId = req.Query["deviceId"];
+            var device = await registryManager.GetDeviceAsync(deviceId);
+            if (device != null)
+                return new OkObjectResult(device.ConnectionState.ToString());
+
+            return new BadRequestResult();
+        }
+    }
+}
---
+++ Device.IntelliFanLekt6/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Device.IntelliFanLekt6
+{
+    enum ConnectionState
+    {
+        NotConnected,
+        Connecting,
+        StillConnecting,
+        Initializing,
+        Connected
+    }
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ConnectAsync().ConfigureAwait(false);
+        }
+
+        private int interval = 10000;
+        private bool isConnected;
+        private string deviceId = "intellifan-e001";
+        private DeviceClient deviceClient;
+
+        private void SetConnectionState(ConnectionState state)
+        {
+
+        }
+
+        private async Task ConnectAsync()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (!isConnected)
+                {
+                    // If more then 3 attempts then change connectionState
+                    SetConnectionState(i > 3 ? ConnectionState.StillConnecting : ConnectionState.Connecting);
+
+                    using (var http = new HttpClient())
+                    {
+                        var response = await http.PostAsJsonAsync("https://kyh-shared.azurewebsites.net/api/devices/connect", new HttpDeviceRequest { DeviceId = deviceId });
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var data = JsonConvert.DeserializeObject<HttpDeviceResponse>(await response.Content.ReadAsStringAsync());
+                            if (data != null)
+                            {
+                                try
+                                {
+                                    deviceClient = DeviceClient.CreateFromConnectionString(data.ConnectionString);
+
+                                    SetConnectionState(ConnectionState.Initializing);
+                                    var twinCollection = new TwinCollection();
+                                    twinCollection["owner"] = "Hans";
+                                    twinCollection["deviceType"] = "FAN";
+
+                                    await deviceClient.UpdateReportedPropertiesAsync(twinCollection);
+                                    var twin = deviceClient.GetTwinAsync();
+
+                                    var result = await http.GetAsync($"https://kyh-shared.azurewebsites.net/api/devices/connect?code=-SWiKVko3aRE3PV53iizVYEXzE84M8hpmxI39vqvXxqfAzFus0uGDA==&deviceId={deviceId}");
+                                    if (result.IsSuccessStatusCode)
+                                    {
+                                        var connectionstate = await result.Content.ReadAsStringAsync();
+                                        if (connectionstate == "Connected")
+                                        {
+                                            SetConnectionState(ConnectionState.Connected);
+                                            break;
+                                        }
+                                    }
+                                }
+                                catch { }
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+        private void btnToggle_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
